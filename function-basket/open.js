1a5d99dc6262ff3fbc470bc7584880aa613d95c8
chee <chee@snoot.club>
2019-01-28 18:01:03 +0000

    Add a debug mode that prints URL

--- function-basket/open.js
+++ function-basket/open.js
@@ -1,9 +1,14 @@
 import {spawn} from "child_process"
+import yargs from "yargs"
 
 export default async function open (url) {
 	let child = spawn("open", [url], {
 		stdio: [0, 1, 2]
 	})
+
+	if (yargs.argv.debug) {
+		console.log({url})
+	}
 
 	return new Promise((resolve, reject) => {
 		child.on("error", reject)
